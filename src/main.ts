8a5976a219eb217d163fda89a169a1d8a9ca3940
Patrick Roelofs <contact@patrickroelofs.com>
2023-10-23 19:40:42 +0200

    add custom css and js to api

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,14 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/', app, document);
+  SwaggerModule.setup('/', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.9.0/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.9.0/swagger-ui-standalone-preset.js',
+    ],
+  });
 
   await app.listen(3000);
 }
