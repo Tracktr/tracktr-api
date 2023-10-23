e6b9aa9eef4ba4f4a19410e6141bb60d9720307a
Patrick Roelofs <contact@patrickroelofs.com>
2023-10-23 20:13:00 +0200

    setup config

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,16 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Tracktr API')
-    .setDescription('Tracktr API description')
-    .setVersion('0.1')
-    .build();
+  if (process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('Tracktr API')
+      .setDescription('Tracktr API description')
+      .setVersion('0.1')
+      .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/', app, document, {
-    customCssUrl:
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css',
-    customJs: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.9.0/swagger-ui-bundle.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.9.0/swagger-ui-standalone-preset.js',
-    ],
-  });
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/', app, document);
+  }
 
   await app.listen(3000);
 }
