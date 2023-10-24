16cdead9c76d08e21d3430dd84868885f29348ed
Patrick Roelofs <contact@patrickroelofs.com>
2023-10-24 19:44:42 +0200

    error handling around fetch

--- src/modules/tmdb/tmdb.service.ts
+++ src/modules/tmdb/tmdb.service.ts
@@ -1,11 +1,36 @@
-import { Get, Injectable, Param } from '@nestjs/common';
+import {
+  Get,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  Param,
+} from '@nestjs/common';
 import { Movie } from './interfaces/movie.interface';
-import { fetchFromTMDB } from '../../lib/fetch/tmdbFetch';
 
 @Injectable()
 export class TMDBService {
   @Get('id/:id')
   async getMovieById(@Param('id') id: string): Promise<Movie> {
-    return await fetchFromTMDB<Movie>('movie', `${id}?language=en-US`);
+    const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${
+          process.env.TMDB_AUTHORIZATION_TOKEN || process.env.TMDB_API_KEY
+        }`,
+      },
+    };
+
+    return fetch(url, options).then((response) => {
+      if (!response.ok) {
+        throw new HttpException(
+          'Error fetching from TMDB',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      return response.json() as Promise<Movie>;
+    });
   }
 }
